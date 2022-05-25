c02809beb93bd43609cad80cb8c62bdbf9fc97f5
lex3man <lex3man@gmail.com>
2022-05-25 15:40:55 +0300

    some lines fixed

--- lexer/Users_assets/views.py
+++ lexer/Users_assets/views.py
@@ -25,64 +25,63 @@ class CreateUser(View):
             'status':'error',
             'msg':'auth faild'
         }
-        if auth_check(authentication.get_authorization_header(request).split()):
         
-            data.update({"msg":"Can't create user"})
-            json_body = json.loads(request.body)
-            head = json_body.get('head')
-            
-            if head == 'new_user_start':
-                get_bot_name = json_body.get('bot_name')
-                get_usr_id = json_body.get('usr_id')
-                get_usr_tg = json_body.get('teleg')
-                get_usr_ref = json_body.get('usr_tag')
-                get_usr_name = json_body.get('usr_name')
-                ref_code = 'None'
-                pref = get_usr_ref
-                if len(get_usr_ref) > 8:
-                    ref_code = get_usr_ref
-                    pref = 'None'
-                if len(get_usr_ref) > 10: 
-                    ref_code = get_usr_ref.split('_')[1]
-                    pref = get_usr_ref.split('_')[0]
-                try:
-                    default_tag = UserTag.objects.get(tag_id = 'first_touch')
-                    new_user_code = get_ref_code()
-                    tgbot = TgBot.objects.get(caption = get_bot_name)
-                    user = User(
-                        user_ID = new_user_code,
-                        tg_ID = get_usr_id,
-                        name = get_usr_name,
-                        tg_nickname = get_usr_tg,
-                        registration_date = date.today(),
-                        parent_ref_code = ref_code,
-                        ref_code = new_user_code,
-                        from_bot = tgbot
-                    )
-                    user.save()
-                    user.tags.add(default_tag)
-                    user.from_bot = tgbot
-                    data.update({
-                        'status':'OK',
-                        'msg':'Added new start',
-                        'prefix':pref,
-                        'ref':'None'
-                    })
-                    if ref_code != 'None':
-                        try:
-                            parent = User.objects.get(ref_code = ref_code)
-                            new_ref = RefLink(
-                                caption = parent.name + ' / ' + user.name,
-                                date = datetime.now(),
-                                parent = parent,
-                                child = user
-                            )
-                            new_ref.save()
-                            data.update({'ref':new_ref.caption})
-                        except: data.update({'ref':'Error'})
-                            
-                except: data.update({'status':'error', 'msg':'Не получилось зарегистрировать пользователя'})
-                
+        if auth_check(authentication.get_authorization_header(request).split()) == False: return JsonResponse(data)
+        
+        data.update({"msg":"Can't create user"})
+        json_body = json.loads(request.body)
+        head = json_body.get('head')
+        
+        if head == 'new_user_start':
+            get_bot_name = json_body.get('bot_name')
+            get_usr_id = json_body.get('usr_id')
+            get_usr_tg = json_body.get('teleg')
+            get_usr_ref = json_body.get('usr_tag')
+            get_usr_name = json_body.get('usr_name')
+            ref_code = 'None'
+            pref = get_usr_ref
+            if len(get_usr_ref) > 8:
+                ref_code = get_usr_ref
+                pref = 'None'
+            if len(get_usr_ref) > 10: 
+                ref_code = get_usr_ref.split('_')[1]
+                pref = get_usr_ref.split('_')[0]
+            try:
+                default_tag = UserTag.objects.get(tag_id = 'first_touch')
+                new_user_code = get_ref_code()
+                tgbot = TgBot.objects.get(caption = get_bot_name)
+                user = User(
+                    user_ID = new_user_code,
+                    tg_ID = get_usr_id,
+                    name = get_usr_name,
+                    tg_nickname = get_usr_tg,
+                    registration_date = date.today(),
+                    parent_ref_code = ref_code,
+                    ref_code = new_user_code,
+                    from_bot = tgbot
+                )
+                user.save()
+                user.tags.add(default_tag)
+                user.from_bot = tgbot
+                data.update({
+                    'status':'OK',
+                    'msg':'Added new start',
+                    'prefix':pref,
+                    'ref':'None'
+                })
+                if ref_code != 'None':
+                    try:
+                        parent = User.objects.get(ref_code = ref_code)
+                        new_ref = RefLink(
+                            caption = parent.name + ' / ' + user.name,
+                            date = datetime.now(),
+                            parent = parent,
+                            child = user
+                        )
+                        new_ref.save()
+                        data.update({'ref':new_ref.caption})
+                    except: data.update({'ref':'Error'})
+            except: data.update({'status':'error', 'msg':'Не получилось зарегистрировать пользователя'})
         return JsonResponse(data)
     
     def get(self, request):
