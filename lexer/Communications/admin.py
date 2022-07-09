7277040ba24777162d71182ceedf0f0a3af61ac6
lex3man <lex3man@gmail.com>
2022-07-09 22:04:11 +0300

    new

--- lexer/Communications/admin.py
+++ lexer/Communications/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from .models import Sending, MessageHistory
 
 class SendingAdmin(admin.ModelAdmin):
-    list_display = ('sending_id', 'create_time', 'sending_time', 'sending_text', 'send_done')
+    list_display = ('sending_id', 'create_time', 'sending_time', 'sending_text', 'in_order', 'send_done')
+    # readonly_fields = ('in_order', 'send_done')
+    search_fields = ('sending_id', 'sending_text', 'sending_time')
+    list_filter = ('creator', 'from_bot', 'in_order', 'send_done')
+    actions = [Sending.shedule]
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ('message_text', 'from_bot', 'user', 'message_time')
+    search_fields = ('message_text', 'user', 'message_time')
+    list_filter = ('from_bot', 'user')
 
 admin.site.register(Sending, SendingAdmin)
-admin.site.register(MessageHistory)
+admin.site.register(MessageHistory, HistoryAdmin)
--- lexer/Communications/models.py
+++ lexer/Communications/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from Users_assets.models import User
 from core.models import TgBot
+from django.contrib import admin
+from datetime import datetime
+import requests
 
 class Sending(models.Model):
     sending_id = models.CharField(verbose_name = 'ID рассылки', max_length = 50)
@@ -9,6 +12,7 @@ class Sending(models.Model):
     create_time = models.DateTimeField(verbose_name = 'Рассылка создана', auto_now = False, auto_now_add = True)
     sending_time = models.DateTimeField(verbose_name = 'Время рассылки', auto_now = False, auto_now_add = False)
     sending_text = models.TextField(verbose_name = 'Текст рассылки')
+    in_order = models.BooleanField(verbose_name = 'В очереди', default = False)
     send_done = models.BooleanField(verbose_name = 'Рассылка исполнена', default = False)
     report = models.TextField(verbose_name = 'Отчёт по произведённой рассылке', default = '', blank = True, null = True)
     
@@ -16,9 +20,17 @@ class Sending(models.Model):
         verbose_name = 'Рассылка'
         verbose_name_plural = 'Рассылки'
     
+    @admin.action(description = 'Активировать рассылку')
+    def shedule(self, queryset):
+        for each in queryset:
+            if each.sending_time <= datetime.now():
+                pass
+            else: 
+                pass
+        queryset.update(in_order = True)
+    
     def sending_done(self):
-        self.send_done = True
-        self.save()
+        self.update(send_done = True)
     
     def __str__(self):
         return self.sending_id
